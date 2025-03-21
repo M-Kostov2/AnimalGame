a3749892f6777232141915beb6859e9219c7fd23
M-Kostov2 <135524907+M-Kostov2@users.noreply.github.com>
2025-03-21 14:33:17 +0200

    Add files via upload

---
+++ Interfaces/IAnimal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Game.Interfaces
+{
+    public interface IAnimal
+    {
+        string Name { get; set; }
+        int Hp { get; set; }
+        int Attack { get; set; }
+        int Category { get; set; }
+        string Habitat { get; set; }
+        int Kilograms { get; set; }
+        List<int> killCount { get; set; }
+
+
+
+     
+    }
+}
