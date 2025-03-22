48999a2c2999257c19792189ff46ff9ad94bea23
M-Kostov2 <135524907+M-Kostov2@users.noreply.github.com>
2025-03-22 16:05:35 +0200

    Add files via upload

---
+++ Classes/animalsOptions.cs
@@ -0,0 +1,21 @@
+﻿using Animal_Game.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Game.Classes
+{
+    public class animalsOptions : IanimalsOptions
+    {
+        private readonly List<IAnimal> Animals;
+
+        public void AddAnimal(IAnimal Animal)
+        {
+            Animals.Add(Animal);
+        }
+
+
+    }
+}
