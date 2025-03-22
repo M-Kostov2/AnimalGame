2b6da0a06ff04c851de676f52b3e9dac16dc1d34
M-Kostov2 <135524907+M-Kostov2@users.noreply.github.com>
2025-03-22 16:02:50 +0200

    Add files via upload

---
+++ Interfaces/IanimalsOptions.cs
@@ -0,0 +1,22 @@
+﻿using Animal_Game.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Game.Interfaces
+{
+    public interface IanimalsOptions
+    {
+        abstract void AddAnimal(IAnimal Animal);
+
+        abstract void RemoveAnimal(IAnimal Animal);
+
+        abstract void AnimalEncounter(IAnimal CurrentAnimal, IAnimal AnimalEncounter);
+
+
+        
+
+    }
+}
