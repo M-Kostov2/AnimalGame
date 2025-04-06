3a37fcea9ec08c1b71fd39a12c5f9472a50956a7
M-Kostov2 <135524907+M-Kostov2@users.noreply.github.com>
2025-04-06 20:16:30 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,24 +7,7 @@ namespace Animal_Game
     {
         static void Main(string[] args)
         { 
-            matrixGeneration Gen = new matrixGeneration();
             Console.InputEncoding = Encoding.UTF8;
-
-            string[,] nn = new string[60, 100];
-            char border = (char)0x25A0;
-          //  nn  = Gen.BordersGeneration(ref nn, border);
-          //  nn = Gen.obstaclesGeneration(ref nn);
-
-            for (int i = 0; i < 60; i++)
-            {
-                for (int j = 0; j < 100; j++)
-                {
-                    Console.Write($"{nn[i,j]} ");
-                }
-                Console.WriteLine();
-            }
-
-
             matrixMovement matrixMovement = new matrixMovement();
             matrixMovement.Movement();
 
